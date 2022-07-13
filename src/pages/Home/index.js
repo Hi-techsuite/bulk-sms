46083ededd436e037e2bda4e141a87371400e30d
kingsley goodluck <goodluckcanhelp@gmail.com>
2022-07-13 01:33:47 +0100

    exodus

---
+++ src/pages/Home/index.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+const Home = () => {
+  const [info, setInfo] = useState({
+    from: "12345",
+    message: "",
+  });
+
+  const setData = (e) => {
+    setInfo({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const sendMessage = () => {
+    alert("response");
+  };
+
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <div>
+          <div className="form-group">
+            <label htmlFor="from"> From: </label>
+            <input
+              id="from"
+              type="text"
+              name="from"
+              placeholder="from"
+              value={info.from}
+              onChange={(e) => setData(e)}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="heading"> Heading: </label>
+            <input id="heading" type="text" placeholder="heading" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="numbers"> Number(s): </label>
+            <input type={"text"} />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="message"> Message body: </label>
+            <input
+              id="message"
+              type={"text"}
+              name="message"
+              value={info.message}
+              onChange={(e) => setData(e)}
+            />
+          </div>
+
+          <div>
+            <button>Cancel</button>
+            <button onClick={sendMessage}> Preview</button>
+          </div>
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
